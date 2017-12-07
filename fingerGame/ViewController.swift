8c34351a33b9f130e248791aabb8c6cd78100215
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2017-12-07 21:38:46 +0500

    Project Setup

--- fingerGame/ViewController.swift
+++ fingerGame/ViewController.swift
@@ -10,16 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var masterOutput: UILabel!
+        
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //userInput.text = " "
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        userInput.text = "love"
+    }
+    
+    
+    
+
+    func randomFingers() -> Int{
+        let randomnumber = arc4random_uniform(5) + 1
+        return Int(randomnumber)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func result(_ sender: UIButton) {
+    
+        var fingers = randomFingers()
+       
+        
+        if fingers == 0{
+            fingers = randomFingers()
+        }
+        
+        
+        
+        
+        print(fingers)
+        //CHECK IF USER HAS ENTERED SOMEHTING OR NOT
+        if let input = userInput.text{
+        
+           //check if string entered can be converted to int
+            if Int(input) != nil{
+                 masterOutput.textColor = UIColor.black
+                if Int(input)! > 5 {
+                    masterOutput.textColor = UIColor.red
+                    masterOutput.text = "No more than 5 fingers"
+                }else{
+                if Int(input) == fingers{
+                   
+                    masterOutput.text = "You guessed right"
+                }else{
+                    masterOutput.text = "Sorry I was holding \(fingers) fingers"
+                    }}
+            }else{
+                masterOutput.textColor = UIColor.red
+                masterOutput.text = "Invalid Input"
+        
+        }
+    
     }
 
-
+    }
 }
 
